db045b21a30a08bfe545fdb564b10a699103385a
Jeff Galbraith <jeff.galbraith@xendra.com>
2020-07-06 09:00:15 -0600

    chore: update to q/app v2.x support

--- demo/quasar.conf.js
+++ demo/quasar.conf.js
@@ -1,7 +1,14 @@
 // Configuration for your app
+// https://quasar.dev/quasar-cli/quasar-conf-js
 
 module.exports = function (ctx) {
   return {
+    // https://quasar.dev/quasar-cli/supporting-ts
+    supportTS: false,
+
+    // https://quasar.dev/quasar-cli/prefetch-feature
+    // preFetch: true,
+
     // app boot file (/src/boot)
     // --> boot files are part of "main.js"
     boot: [
@@ -12,55 +19,38 @@ module.exports = function (ctx) {
     ],
 
     extras: [
-      'roboto-font',
-      'material-icons' // optional, you are not bound to it
       // 'ionicons-v4',
-      // 'mdi-v3',
+      // 'mdi-v4',
       // 'fontawesome-v5',
-      // 'eva-icons'
+      // 'eva-icons',
+      // 'themify',
+      // 'line-awesome',
+      // 'roboto-font-latin-ext', // this or either 'roboto-font', NEVER both!
+
+      'roboto-font', // optional, you are not bound to it
+      'material-icons' // optional, you are not bound to it
     ],
-
-    // framework: 'all', // --- includes everything; for dev only!
-    framework: {
-      components: [
-        'QLayout',
-        'QHeader',
-        'QDrawer',
-        'QPageContainer',
-        'QPage',
-        'QToolbar',
-        'QToolbarTitle',
-        'QBtn',
-        'QIcon',
-        'QList',
-        'QItem',
-        'QItemSection',
-        'QItemLabel'
-      ],
-
-      directives: [
-        'Ripple'
-      ],
-
-      // Quasar plugins
-      plugins: [
-        'Notify'
-      ]
-
-      // iconSet: 'ionicons-v4'
-      // lang: 'de' // Quasar language
-    },
-
-    supportIE: false,
 
     build: {
       scopeHoisting: true,
       vueRouterMode: 'history',
       publicPath: 'app-extension-dotenv',
+      // transpile: false,
+
+      // Add dependencies for transpiling with Babel (Array of string/regex)
+      // (from node_modules, which are by default not transpiled).
+      // Applies only if "transpile" is set to true.
+      // transpileDependencies: [],
+
       // vueCompiler: true,
+      // preloadChunks: true,
+      // showProgress: false,
       // gzip: true,
       // analyze: true,
+
+      // Options below are automatically set depending on the env, set them if you want to override
       // extractCSS: false,
+
       extendWebpack (cfg) {
         cfg.module.rules.push({
           enforce: 'pre',
@@ -72,9 +62,27 @@ module.exports = function (ctx) {
     },
 
     devServer: {
-      // https: true,
+      https: false,
       // port: 8080,
       open: true // opens browser window automatically
+    },
+    // framework: 'all', // --- includes everything; for dev only!
+    framework: {
+      iconSet: 'material-icons', // Quasar icon set
+      lang: 'en-us', // Quasar language pack
+      config: {
+        dark: 'auto'
+      },
+
+      // Possible values for "importStrategy":
+      // * 'auto' - (DEFAULT) Auto-import needed Quasar components & directives
+      // * 'all'  - Manually specify what to import
+      importStrategy: '',
+
+      // Quasar plugins
+      plugins: [
+        'Notify'
+      ]
     },
 
     // animations: 'all' --- includes all animations
@@ -97,27 +105,27 @@ module.exports = function (ctx) {
         theme_color: '#027be3',
         icons: [
           {
-            src: 'statics/icons/icon-128x128.png',
+            src: 'icons/icon-128x128.png',
             sizes: '128x128',
             type: 'image/png'
           },
           {
-            src: 'statics/icons/icon-192x192.png',
+            src: 'icons/icon-192x192.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: 'statics/icons/icon-256x256.png',
+            src: 'icons/icon-256x256.png',
             sizes: '256x256',
             type: 'image/png'
           },
           {
-            src: 'statics/icons/icon-384x384.png',
+            src: 'icons/icon-384x384.png',
             sizes: '384x384',
             type: 'image/png'
           },
           {
-            src: 'statics/icons/icon-512x512.png',
+            src: 'icons/icon-512x512.png',
             sizes: '512x512',
             type: 'image/png'
           }
