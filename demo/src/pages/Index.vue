9b61f242ded5644f51146eff175807ea7a16bf47
Jeff Galbraith <jeff.galbraith@xendra.com>
2021-02-16 12:44:46 -0700

    feat: expanded environmental variables

--- demo/src/pages/Index.vue
+++ demo/src/pages/Index.vue
@@ -1,7 +1,14 @@
 <template>
   <q-page class="flex flex-center">
     <div class="col-lg-3 col-sm-6 col-sm-10 col-xs-12">
-      <div>Open the browser Dev Tools, then find "Dotenv Test: (TEST: 1)"</div>
+      <div>Open the browser Dev Tools, then find "Dotenv Test:"</div>
+      <div>
+        <pre>
+{{ JSON.stringify(output, null, 2)}}
+        </pre>
+      </div>
+      <div>SHELL is coming from an environmental variable inside the .env file</div>
+      <div>SHELL=${SHELL}</div>
     </div>
   </q-page>
 </template>
@@ -11,6 +18,14 @@
 
 <script>
 export default {
-  name: 'PageIndex'
+  name: 'PageIndex',
+  data () {
+    return {
+      output: {
+        TEST: process.env.TEST,
+        SHELL: process.env.SHELL
+      }
+    }
+  }
 }
 </script>
